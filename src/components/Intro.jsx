3ed8184931bcaa976df1854f054b4c20f3999c2e
ornenovino <istar.novino@gmail.com>
2024-03-15 01:32:56 -0300

    img testing intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,7 @@ function Intro() {
                     “Al otro día te ponías ese uniforme de invencible y volvías a salir a luchar”
                 </div>
                 <div className="relative w-full md:w-auto flex justify-end items-end md:pr-8">
-                    <img className="bottom-0 right-0 md:max-w-[120%] md:right-auto -mb-4 md:-mb-8" src="/public/assets/vizintin-main.png" alt="tintin" />
+                    <img className="bottom-0 right-0 md:max-w-[120%] md:right-auto -mb-4 md:-mb-8" src="/assets/vizintin-main.png" alt="tintin" />
                 </div>
             </div>
         </article>
