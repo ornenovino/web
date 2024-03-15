1d677d3d854f73183396002aa07d4109ad8ee067
ornenovino <istar.novino@gmail.com>
2024-03-15 01:57:17 -0300

    font upload

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,9 +3,9 @@ function Intro() {
         <article className="flex items-center justify-center mx-auto p-4">
             <div className="relative w-full max-w-6xl bg-zinc-300 rounded-3xl flex flex-col md:flex-row items-center justify-between p-4 md:p-8">
                 <div
-                    className="z-10 text-indigo-950 italic text-2xl md:text-5xl font-bold font-['Volkhov'] leading-tight md:leading-12 mb-4 md:mb-0 md:pl-4"
+                    className="z-10 text-indigo-950  text-2xl md:text-5xl font-bold font-['Volkhov'] leading-tight md:leading-12 mb-4 md:mb-0 md:pl-4"
                 >
-                    “Al otro día te ponías ese uniforme de invencible y volvías a salir a luchar”
+                    <p className="tracking-wide">“Al otro día te ponías ese uniforme de invencible y volvías a salir a luchar”</p>
                 </div>
                 <div className="relative w-full md:w-auto flex justify-end items-end md:pr-8">
                     <img className="bottom-0 right-0 md:max-w-[120%] md:right-auto -mb-4 md:-mb-8" src="assets/vizintin-main.png" alt="tintin" />
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        volkov: ["Volkov", "sans-serif"]
+      },
+    },
+  },
   plugins: [react()],
   base: '/web/'
 })
